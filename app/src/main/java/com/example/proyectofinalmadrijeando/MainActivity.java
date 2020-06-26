49b8e9283bad8ecdb8cd5e0c92910e12f899d04f
Ezequiel <ezedifo03@gmail.com>
2020-06-26 11:46:35 -0300

    12/6

--- app/src/main/java/com/example/proyectofinalmadrijeando/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalmadrijeando/MainActivity.java
@@ -16,30 +16,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> ListDatos;
-    RecyclerView Recicler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
-        Recicler=findViewById(R.id.ReciclerId);
-        Recicler.setLayoutManager(new LinearLayoutManager(this ));
-        ListDatos=new ArrayList<String>();
-        for(int i=0; i<=10;i++){
-            ListDatos.add("Dato #" + i + "Dato");
-        }
+        setContentView(R.layout.activity_main);
+
         //
-        AdapterDatos Adapter = new AdapterDatos(ListDatos);
-        Recicler.setAdapter(Adapter);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
+
+ }
 
 }
--- app/src/main/java/com/example/proyectofinalmadrijeando/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/proyectofinalmadrijeando/ui/home/HomeFragment.java
@@ -11,11 +11,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyectofinalmadrijeando.R;
+import com.example.proyectofinalmadrijeando.ui.AdapterDatos;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-
+    private ArrayList<String> ListDatos;
+    private RecyclerView Recicler;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,6 +30,7 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.txtMateriales);
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -31,5 +38,20 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Recicler=getActivity().findViewById(R.id.ReciclerId);
+        Recicler.setLayoutManager(new LinearLayoutManager(getContext() ));
+        ListDatos=new ArrayList<String>();
+        for(int i=0; i<=10;i++){
+            ListDatos.add("Dato #" + i + "Dato");
+        }
+        AdapterDatos Adapter = new AdapterDatos(ListDatos);
+        Recicler.setAdapter(Adapter);
     }
 }
